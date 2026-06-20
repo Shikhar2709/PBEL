498c48c26a180db132a12260969a51113b1b20f0
Shikhar2709 <srivastava.shikhar2709@gmail.com>
2026-06-20 13:56:47 +0530

    Last updated files

---
+++ basics.py
@@ -0,0 +1,30 @@
+# variable
+a = 10
+print(a) #integer variable
+print(type(a))
+a = 10.5 #float variable
+print(a)
+print(type(a))
+a = "Hello World" #string variable ' ' or " " or """ """ can be used for string
+print(a)
+print(type(a))
+a= True #boolean variable
+print(a)
+print(type(a))
+a=1+2j #complex variable
+print(a)
+print(type(a))
+a= [1,2,3,4,5] #list variable
+print(a)    
+print(type(a))
+a= (1,2,3,4,5) #tuple variable
+print(a)
+print(type(a))
+a= {1,2,3,4,5} #set variable
+print(a)
+print(type(a))
+a= {"name":"John", "age":30, "city":"New York"} #dictionary variable
+print(a)
+print(type(a))
+class MyClass: #class variable
+    pass
